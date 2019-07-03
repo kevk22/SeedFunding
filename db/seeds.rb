dd5fbbbe6593db79ca09c26da94e61b00f572e58
Kevin <kevinkumar90@gmail.com>
2019-07-03 09:00:40 -0700

    add user seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+user1 = User.create(name: 'John', email: 'john1@aa.io', password: 'password')
+user2 = User.create(name: 'Jimmy', email: 'jimmy2@aa.io', password: 'password')
+user3 = User.create(name: 'Kevin', email: 'kevin3@aa.io', password: 'password')
+user4 = User.create(name: 'Elliot', email: 'elliot4@aa.io', password: 'password')
+user5 = User.create(name: 'Jason', email: 'jason5@aa.io', password: 'password')
+user6 = User.create(name: 'Marc', email: 'marc6@aa.io', password: 'password')
+user7 = User.create(name: 'Spencer', email: 'spencer7@aa.io', password: 'password')
+user8 = User.create(name: 'Jesus', email: 'jesus8@aa.io', password: 'password')
