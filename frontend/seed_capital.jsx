88bf17adf9240fa2aca7eacac6add43780a4afda
Kevin <kevinkumar90@gmail.com>
2019-07-03 10:23:50 -0700

    add session api util and actions

---
+++ frontend/seed_capital.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
+document.addEventListener("DOMContentLoaded", () => {
+    let store = configureStore();
+    ReactDOM.render(<Root store={store}, document.getElementById('root'));
+})
