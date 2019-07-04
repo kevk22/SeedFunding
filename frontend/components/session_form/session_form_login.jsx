7b2e9527c57d0eb4139c7ace92c205e866234e11
Kevin <kevinkumar90@gmail.com>
2019-07-04 14:23:11 -0700

    add animated demo login

--- frontend/components/session_form/session_form_login.jsx
+++ frontend/components/session_form/session_form_login.jsx
@@ -10,7 +10,7 @@ class SessionFormLogin extends React.Component {
       password: ''
     };
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleGuestSubmit = this.handleGuestSubmit.bind(this)
+    // this.handleGuestSubmit = this.handleGuestSubmit.bind(this)
   }
 
   update(field) {
@@ -37,10 +37,41 @@ class SessionFormLogin extends React.Component {
     );
   }
 
-  handleGuestSubmit(e) {
+  // handleGuestSubmit(e) {
+  //   e.preventDefault();
+  //   this.props.demoUser({email: 'demo', password: 'password'})
+  //     .then(() => this.props.history.push("/"));
+  // }
+
+
+
+  async demoLogin(e) {
     e.preventDefault();
-    this.props.demoUser({email: 'demo', password: 'password'})
-      .then(() => this.props.history.push("/"));
+
+    const demoUser = {
+      email: 'demo', password: 'password' 
+    };
+
+    const sleep = ms => new Promise(res => setTimeout(res, ms));
+
+    document.getElementById('email-input').focus();
+    for (let i = 1; i <= demoUser.email.length; i++) {
+      this.setState({ email: demoUser.email.substr(0, i) });
+      await sleep(50);
+    }
+
+    await sleep(250);
+
+    document.getElementById('password-input').focus();
+    for (let i = 1; i <= demoUser.password.length; i++) {
+      this.setState({ password: demoUser.password.substr(0, i) });
+      await sleep(50);
+    }
+
+    await sleep(250);
+
+    document.getElementById('session-submit-btn').click();
+    document.getElementById('password-input').blur();
   }
 
   render() {
@@ -53,7 +84,7 @@ class SessionFormLogin extends React.Component {
           <div className="login-form">
             <br/>
             <label>Email:
-              <input type="text"
+              <input type="text" id="email-input"
                 value={this.state.email}
                 onChange={this.update('email')}
                 className="login-input"
@@ -61,15 +92,15 @@ class SessionFormLogin extends React.Component {
             </label>
             <br/>
             <label>Password:
-              <input type="password"
+              <input type="password" id="password-input"
                 value={this.state.password}
                 onChange={this.update('password')}
                 className="login-input"
               />
             </label>
             <br/>
-            <input className="session-submit" type="submit" value={this.props.formType} />
-            <button onClick={this.handleGuestSubmit}>Login as demo user</button>
+            <input className="session-submit" type="submit" value={this.props.formType} id="session-submit-btn" />
+            <button onClick={this.demoLogin.bind(this)}>Login as demo user</button>
             <br/>
             New to SeedFunding? {this.props.navLink}
           </div>
