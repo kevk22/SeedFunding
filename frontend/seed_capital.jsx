3287ba15f761ee0a9f439b9c3483bad588155001
Kevin <kevinkumar90@gmail.com>
2019-07-03 17:43:56 -0700

    fix dispatch

--- frontend/seed_capital.jsx
+++ frontend/seed_capital.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
-import { login, signup, logout } from './util/session_api_util';
+import { login, signup, logout } from './actions/session_actions';
 
 document.addEventListener("DOMContentLoaded", () => {
     let store = configureStore();
-    // window.login = login;
-    // window.signup = signup;
-    // window.logout = logout;
+    window.login = login;
+    window.signup = signup;
+    window.logout = logout;
 
-    // window.getState = store.getState;
-    // window.dispatch = store.dispatch; 
+    window.getState = store.getState;
+    window.dispatch = store.dispatch; 
 
     
     ReactDOM.render(<Root store={store}/>, document.getElementById('root'));
